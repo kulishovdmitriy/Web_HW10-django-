91012a175a386d5172d3f44541b035bd667b8421
dmitriy <kylishovdimka@ukr.net>
2024-04-02 12:47:48 +0300

    users app

--- home_projects/users/urls.py
+++ home_projects/users/urls.py
@@ -1,9 +1,12 @@
-# from django.urls import path
-#
-# from . import views
-#
-# app_name = 'users'
-#
-# urlpatterns = [
-#     path('', ),
-# ]
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .forms import LoginForm
+
+app_name = 'users'
+
+urlpatterns = [
+    path('signup/', views.RegisterView.as_view(), name='signup'),
+    path('signin/', LoginView.as_view(template_name='users/login.html', form_class=LoginForm), name='signin'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout')
+]
--- home_projects/users/views.py
+++ home_projects/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import RegisterForm
+from django.contrib import messages
 # Create your views here.
+
+
+class RegisterView(View):
+    template_name = 'users/register.html'
+    form_class = RegisterForm
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': self.form_class})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f'Account created for {username}!')
+            return redirect(to='users:signin')
+        return render(request, self.template_name, {'form': form})
