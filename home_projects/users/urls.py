5752a082cee0882fbd8bfd39c7cf1ad6e77ab24b
dmitriy <kylishovdimka@ukr.net>
2024-04-02 20:32:08 +0300

    Init DB

--- home_projects/users/urls.py
+++ home_projects/users/urls.py
@@ -7,6 +7,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.RegisterView.as_view(), name='signup'),
-    path('signin/', LoginView.as_view(template_name='users/login.html', form_class=LoginForm), name='signin'),
+    path('signin/', LoginView.as_view(template_name='users/login.html', form_class=LoginForm,
+                                      redirect_authenticated_user=True), name='signin'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
---
+++ home_projects/utils/migration.py
@@ -0,0 +1,48 @@
+import os
+import django
+
+from pymongo import MongoClient
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_projects.settings')
+django.setup()
+
+from quotes.models import Quote, Tag, Author
+
+client = MongoClient(
+    'mongodb+srv://dmitriyykulishov:52628271@mongodb.dtj8cur.mongodb.net/?retryWrites=true&w=majority&appName=mongoDB'
+)
+db = client.mongodb
+
+authors = db.authors.find()
+
+for author in authors:
+    Author.objects.get_or_create(
+        fullname=author['fullname'],
+        born_date=author['born_date'],
+        born_location=author['born_location'],
+        description=author['description']
+    )
+
+quotes = db.quotes.find()
+
+for quote in quotes:
+    tags = []
+    for tag in quote['tags']:
+        t, *_ = Tag.objects.get_or_create(name=tag)
+        tags.append(t)
+
+    exist_quote = bool(len(Quote.objects.filter(quote=quote['quote'])))
+
+    if not exist_quote:
+        author = db.authors.find_one({'_id': quote['author']})
+        author_name = quote['author']
+        a = Author.objects.filter(fullname=author_name).first()
+        if a is not None:
+            q = Quote.objects.create(
+                quote=quote['quote'],
+                author=a
+            )
+            for tag in tags:
+                q.tags.add(tag)
+        else:
+            print(f"Author '{author_name}' not found.")
